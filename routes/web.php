f69356efdaa145a573c519d60b27e44b3351de4c
haakonaareskjold <aareskjold.hhaakon@gmail.com>
2021-03-08 20:21:46 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->basePath();
+});
+
+$router->get('user/{id}', fn($id) => \App\Models\User::query()->findOrFail($id));
+
+$router->get('user/view/{id}', 'ExampleController@show');
